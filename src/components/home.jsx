0a4d9d8d2523a1e20ab9aea45d783c671448b43f
Harish <harishpoctor@gmail.com>
2022-09-06 21:09:37 +0530

    First draft of home page containing list of apps complete.d

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import appsService from "../services/appsService";
 
 class Home extends Component {
+  state = { apps: [] };
+
+  async componentDidMount() {
+    const apps = await appsService.getApps();
+    console.log("apps :>> ", apps);
+    this.setState({ apps });
+  }
+
   render() {
-    return <div className="container"></div>;
+    return (
+      <div className="container container_center">
+        {this.state.apps.map((item) => (
+          <div className="app-details list-group-item">
+            <div
+              onClick={() => (window.location.href = item.link)}
+              className="logo-title-link d-flex align-items-center flex-column c-pointer"
+            >
+              <img
+                className="img-thumbnail home-page-thumbnail"
+                src={item.logo_url}
+                alt={item.name}
+              />
+              <a className="stretched-link" href={item.link} target="_blank">
+                {item.name}
+              </a>
+            </div>
+            <div className="description text-center">
+              {item.description.length >= 100
+                ? item.description.substring(0, 100) + "..."
+                : item.description}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
