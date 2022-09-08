c1ba3ed8afce6bcfa64c5dfcb2309a7fb9d63564
Harish <harishpoctor@gmail.com>
2022-09-08 13:08:34 +0530

    Using Openseadragon library for viewing image.

--- src/components/mindmapImageView.jsx
+++ src/components/mindmapImageView.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { getMindmap } from "../services/mindmapService";
 import SpinnerWhileLoading from "./common/spinnerWhileLoading";
+import OpenSeadragon from "openseadragon/build/openseadragon/openseadragon";
 
 function MindmapImageView(props) {
   const mindmapId = props.match.params.id;
+  const [viewer, setViewer] = useState(null);
 
   const [data, setData] = useState({
     title: "",
@@ -31,6 +33,21 @@ function MindmapImageView(props) {
   }
 
   useEffect(() => {
+    if (viewer && data.image_link) {
+      viewer.open({ type: "image", url: data.image_link });
+    } else if (!viewer && data.image_link) {
+      setViewer(
+        OpenSeadragon({
+          id: "openseadragon1",
+          prefixUrl:
+            "https://cdn.jsdelivr.net/npm/openseadragon@3.1/build/openseadragon/images/",
+          tileSources: { type: "image", url: data.image_link },
+        })
+      );
+    }
+  }, [data]);
+
+  useEffect(() => {
     loadMindmap();
   }, []);
 
@@ -40,13 +57,9 @@ function MindmapImageView(props) {
         <div>
           Mindmap Image view : {data.title} ({data.category})
         </div>
-        <a
-          href={data.image_link}
-          data-lightbox="mindmap-image"
-          data-title={`Mindmap image of ${data.title}`}
-        >
-          Open image
-        </a>
+        <div className="h-75 w-100">
+          <div id="openseadragon1" className="w-100 h-100"></div>
+        </div>
       </SpinnerWhileLoading>
     </div>
   );
